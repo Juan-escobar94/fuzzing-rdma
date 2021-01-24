ccad449a97fdd28f325271eb6fab5775a2d19bfb
Juan Osorio <juanosorio94@github.com>
2021-01-24 22:28:20 +0100

    delete moved files

--- key_based_improved/qp_transitions.h
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-
-#ifndef QP_TRANSITIONS
-#define QP_TRANSITIONS
-int qp_reset_to_init(struct ibv_qp *qp, const int port_num);
-int qp_init_to_rtr(struct ibv_qp *qp, union ibv_gid gid, uint32_t dest_qp, uint16_t destination_local_id, uint8_t port_num);
-int qp_rtr_to_rts(struct ibv_qp *qp);
-#endif /* QP_TRANSITIONS */
--- set_devices.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo rdma link add rxe0 type rxe netdev enp0s3
